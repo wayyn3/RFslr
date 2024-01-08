547edf4c44272e7ac677691289230650339037c7
wayyn3 <136890021+wayyn3@users.noreply.github.com>
2024-01-08 21:25:27 +0800

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import cv2
 import mediapipe as mp
 import numpy as np
 from tensorflow import keras
-import pickle
+import pickle 
 from streamlit_webrtc import webrtc_streamer, VideoProcessorBase, WebRtcMode
 import av
 
@@ -15,30 +15,25 @@ mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
 hands = mp_hands.Hands(static_image_mode=True, min_detection_confidence=0.3)
 
-labels_dict = {0: 'A', 1: 'B', 2: 'C', 3: 'D', 4: 'E', 5: 'F', 6: 'G', 7: 'H', 8: 'I', 9: 'J',
-               10: 'K', 11: 'L', 12: 'M', 13: 'N', 14: 'O', 15: 'P', 16: 'Q', 17: 'R', 18: 'S',
+labels_dict = {0: 'A', 1: 'B', 2: 'C', 3: 'D', 4: 'E', 5: 'F', 6: 'G', 7: 'H', 8: 'I', 9: 'J', 
+               10: 'K', 11: 'L', 12: 'M', 13: 'N', 14: 'O', 15: 'P', 16: 'Q', 17: 'R', 18: 'S', 
                19: 'T', 20: 'U', 21: 'V', 22: 'W', 23: 'X', 24: 'Y', 25: 'Z'}
-
-flipped_frame = None
-x_ = []
-y_ = []
 
 
 class VideoProcessor(VideoProcessorBase):
     def recv(self, frame: av.VideoFrame) -> av.VideoFrame:
-        global flipped_frame, x_, y_
         img = frame.to_ndarray(format="bgr24")
         flipped_frame = cv2.flip(img, 1)
         H, W, _ = flipped_frame.shape
         frame_rgb = cv2.cvtColor(flipped_frame, cv2.COLOR_BGR2RGB)
         results = hands.process(frame_rgb)
-
+        
         if results.multi_hand_landmarks:
             for hand_landmarks in results.multi_hand_landmarks:
                 mp_drawing.draw_landmarks(
-                    flipped_frame,
-                    hand_landmarks,
-                    mp_hands.HAND_CONNECTIONS,
+                    flipped_frame,  
+                    hand_landmarks,  
+                    mp_hands.HAND_CONNECTIONS,  
                     mp_drawing_styles.get_default_hand_landmarks_style(),
                     mp_drawing_styles.get_default_hand_connections_style())
 
@@ -115,12 +110,9 @@ st.title('Sign Language Recognition App with MediaPipe')
 app_mode = st.sidebar.selectbox('Select Mode', ['About App', 'Run with Image', 'Run in Real Time'])
 
 if app_mode == 'About App':
-    st.markdown(
-        'This app performs real-time ASL Alphabet sign language recognition using a pre-trained machine learning model with MediaPipe hand detection.')
-    st.markdown(
-        'The app is divided into two modes: "Run in Real Time" and "Run with Image." In the "Run in Real Time" mode, the app initializes the webcam and utilizes the MediaPipe Hands module to detect hand landmarks in each frame. The detected landmarks are then processed to obtain the required features, and the pre-trained Random Forest model is used to predict the corresponding sign language gesture. The "Run with Image" mode allows users to upload an image for sign language recognition. The uploaded image is processed using the same pipeline as in the real-time mode, and the predicted gesture is displayed alongside the original image.')
-    st.markdown(
-        'The dataset is taken from Kaggle: https://www.kaggle.com/datasets/lexset/synthetic-asl-alphabet')
+    st.markdown('This app performs real-time ASL Alphabet sign language recognition using a pre-trained machine learning model with MediaPipe hand detection.')
+    st.markdown('The app is divided into two modes: "Run in Real Time" and "Run with Image." In the "Run in Real Time" mode, the app initializes the webcam and utilizes the MediaPipe Hands module to detect hand landmarks in each frame. The detected landmarks are then processed to obtain the required features, and the pre-trained Random Forest model is used to predict the corresponding sign language gesture. The "Run with Image" mode allows users to upload an image for sign language recognition. The uploaded image is processed using the same pipeline as in the real-time mode, and the predicted gesture is displayed alongside the original image.')
+    st.markdown('The dataset is taken from Kaggle: https://www.kaggle.com/datasets/lexset/synthetic-asl-alphabet')
     st.header('Video Demo - Run In Real Time')
     local_video_path = './RealTimeDemo.mp4'
     st.video(local_video_path)
@@ -135,12 +127,16 @@ elif app_mode == 'Run with Image':
     if image_file_buffer:
         image = cv2.imdecode(np.frombuffer(image_file_buffer.read(), np.uint8), 1)
         sign_language_recognition_on_image(image)
-        
-elif app_mode == 'Run in Real Time':
-    st.write('Perform the sign language gesture in front of your webcam...')
 
+elif app_mode == 'Run in Real Time':
     webrtc_ctx = webrtc_streamer(
         key="example",
         mode=WebRtcMode.SENDRECV,
         video_processor_factory=VideoProcessor
     )
+    st.write('Perform the sign language gesture in front of your webcam...')
+    if webrtc_ctx.video_processor:
+        while True:
+            frame = webrtc_ctx.video_processor.recv()
+            if frame is not None:
+                st.image(frame.to_ndarray(format="bgr24"), channels='BGR', use_column_width=True)
